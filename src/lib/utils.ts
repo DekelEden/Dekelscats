e9b379b3856a7779695ae91555a9e36026f45ef0
Chen Asraf <casraf@pm.me>
2024-04-16 17:18:48 +0300

    feat: contact form on index

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,8 @@ import type { ClassNameValue } from "tailwind-merge"
 import clsx from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export type ClassMap<T extends string> = Partial<Record<T, string>>
+
 export function cn(...values: ClassNameValue[]): string {
   return twMerge(clsx(...values))
 }
